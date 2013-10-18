25a868e23e0934644e9061ac8f48b3d3af9ba00c
tylerstauss <tylerstauss@Tylers-Mac-mini.local>
2013-10-18 15:23:11 -0700

    37Signals
    Initial upload of all files

---
+++ application.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	  $('#center-left').hover(function(){
+   $('#left').toggleClass('hidden');
+   $('.hide').toggleClass('hidden');
+});
+   $('#center-center').hover(function(){
+   $('#center').toggleClass('hidden');
+   $('.hide').toggleClass('hidden');
+});
+   $('#center-right').hover(function(){
+   $('#right').toggleClass('hidden');
+   $('.hide').toggleClass('hidden');
+});
+
+
+
+});
